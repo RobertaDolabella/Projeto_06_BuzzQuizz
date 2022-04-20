a4e0702283817e91cbb5c6b4f51ae23d5823cfe7
RobertaDolabella <dolabella.roberta@gmail.com>
2022-04-20 13:30:15 -0300

    1. Layout básico e botao adicionar direcionando para tela 'começar pelo começo'

---
+++ java.js
@@ -0,0 +1,6 @@
+function sumirBotao(){
+    document.querySelector(".botao-criacao").classList.add("off")
+    document.querySelector(".container-criacao").classList.remove("off")
+    document.querySelector(".container-principal").classList.add("off")
+    document.querySelector(".comece-pelo-comeco").classList.remove("off")
+}
